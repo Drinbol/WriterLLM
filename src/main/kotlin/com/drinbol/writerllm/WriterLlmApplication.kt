917be18cf2e2b9ac82d716de0bffb0df6a5e9a45
Drinbol <drinbol@outlook.com>
2024-05-16 00:13:42 +1000

    create project

---
+++ src/main/kotlin/com/drinbol/writerllm/WriterLlmApplication.kt
@@ -0,0 +1,11 @@
+package com.drinbol.writerllm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WriterLlmApplication
+
+fun main(args: Array<String>) {
+    runApplication<WriterLlmApplication>(*args)
+}
---
+++ src/test/kotlin/com/drinbol/writerllm/WriterLlmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.drinbol.writerllm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WriterLlmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
